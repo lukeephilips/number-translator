ba8c7c431151d57ea97402eebdd7aee9ae39a1f5
Ezra Sandzer-Bell and Luke Philips <luke@>
2016-11-15 11:20:41 -0800

    passes all tests through 19

--- lib/number_translator.rb
+++ lib/number_translator.rb
@@ -2,8 +2,8 @@
 
 class Fixnum
   define_method(:translate) do
-    # onesHash = Hash.new()
-    onesHash = {"1" => "one",
+    # OnesHash = Hash.new()
+    OnesHash = {"1" => "one",
                 "2" => "two",
                 "3" => "three",
                 "4" => "four",
@@ -13,12 +13,12 @@ class Fixnum
                 "8" => "eight",
                 "9" => "nine",
                 "0" => "" }
-      preteensHash = {
+      PreteensHash = {
                 "10" => "ten",
                 "11" => "eleven",
                 "12" => "twelve"}
-      prefixHash = {
-                "2" => "twe",
+      PrefixHash = {
+                "2" => "twen",
                 "3" => "thir",
                 "4" => "four",
                 "5" => "fif",
@@ -29,24 +29,30 @@ class Fixnum
 
 
     num_array= []
-    outputWord= ""
+    output_word= ""
     num_array = self.to_s().split("").reverse!()
     length = num_array.length()
 
+    ones_digit = OnesHash.fetch(num_array[0])
+    if length>1
+      tens_digit = ""
+      tens_digit.concat(num_array[1]+ num_array[0])
+      tens_digit_int = tens_digit.to_i
 
-    teenTest = num_array[1].concat(num_array[0])
-    teenTestInt = teenTest.to_i
-
-    if teenTestInt>=10 and teenTestInt<=12
-      outputWord = preteensHash.fetch(teenTest)
-    elsif teenTestInt>=13 and teenTestInt<=19
-      outputWord = prefixHash.fetch(teenTest)+"teen"
+      if tens_digit_int>=10 and tens_digit_int<=12
+        output_word = PreteensHash.fetch(tens_digit)
+      elsif tens_digit_int>=13 and tens_digit_int<=19
+        output_word = PrefixHash.fetch(num_array[0])+"teen"
+      elsif num_array[1].to_i>=2
+        output_word = PrefixHash.fetch(num_array[1])+"ty"
+      end
+    else
+    output_word = ones_digit
     end
-
-    outputWord
     # num_array.each.with_index() do |number, index|
     #
     # end
     # num_array
+    output_word
   end
 end
