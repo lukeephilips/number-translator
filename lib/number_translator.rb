69c23a16f3b685fb51a5d941f21b43c903bc97f8
Luke <luke.e.philips@gmail.com>
2016-11-20 13:59:06 -0800

    refactor both methods

--- lib/number_translator.rb
+++ lib/number_translator.rb
@@ -10,11 +10,13 @@ class String
     end
 
     num_array.each_slice(3).to_a.each.with_index() do |array, i|
-      if i > 0
-        output_array.push(magnitudeSyntax[i-1])
-      end
-      if array
-        output_array.push(array.three_digit_test())
+      if array != (['0','0','0'])
+        if i > 0 and array
+          output_array.push(magnitudeSyntax[i-1])
+        end
+        if array
+          output_array.push(array.three_digit_test())
+        end
       end
     end
 
@@ -28,56 +30,47 @@ end
 class Array
   define_method(:three_digit_test) do
     # onesHash = Hash.new()
-    onesHash = {"1" => "one","2" => "two", "3" => "three","4" => "four","5" => "five","6" => "six","7" => "seven","8" => "eight","9" => "nine","0" => "" }
+    onesHash = {"1" => "one","2" => "two", "3" => "three","4" => "four","5" => "five","6" => "six","7" => "seven","8" => "eight","9" => "nine",
+    "0" => "" }
 
-    preteensHash = {"10" => "ten","11" => "eleven","12" => "twelve"}
+    teensHash = {"10" => "ten","11" => "eleven","12" => "twelve","13" => "thirteen","14" => "fourteen","15" => "fifteen","16" => "sixteen","17" => "seventeen","18" => "eighteen",
+    "19" => 'nineteen'}
 
     prefixHash = {"2" => "twen", "3" => "thir","4" => "four","5" => "fif","6" => "six","7" => "seven","8" => "eight","9" => "nine"}
-    magnitudes = ['teen','ty','hundred']
-    num_array= []
+
+    magnitudes = ['ty','hundred']
+
+    assign_tens = Proc.new {|number|
+      output = ""
+      tens_array=[]
+      if number.to_i == 0
+        output = ""
+      elsif number.to_i < 10
+        output = onesHash.fetch(number.to_i.to_s)
+      end
+      if number.to_i.between?(10,19)
+        output = teensHash.fetch(number)
+      elsif number.to_i >=20
+        tens_array.push(prefixHash.fetch(number[0]) + magnitudes[0])
+        tens_array.push(assign_tens.call(number[1]))
+        output = tens_array.join(" ").rstrip()
+      end
+      output
+    }
+
     output_word= ""
-    num_array = self
-    ones_word = onesHash.fetch(num_array[0])
-    tens_word = ""
-    hundreds_word = ""
 
-    if num_array.length() >= 2
-      tens_digit = ""
-      tens_digit_int = tens_digit.concat(num_array[1]+ num_array[0]).to_i
+    if self == ['0']
+      output_word = "zero"
+    else
+      ones_and_tens_combined = self.each_slice(2).to_a
+      ones_and_tens_combined.map!{|n| n.reverse.join("")}
 
-        if tens_digit_int.between?(10, 12)
-          tens_word = preteensHash.fetch(tens_digit)
-        elsif tens_digit_int.between?(12, 19)
-          tens_word = prefixHash.fetch(num_array[0]) + "teen"
-        end
-
-        if num_array[1].to_i >= 2 and num_array[0] == "0"
-          tens_word = prefixHash.fetch(num_array[1]) + "ty"
-
-        elsif num_array[1].to_i >= 2
-          tens_word = prefixHash.fetch(num_array[1]) + "ty" + " " + ones_word
-        end
-        output_word = tens_word
-    end
-
-    if num_array.length() >= 3
-      if num_array[1] == "0" and num_array[0] == "0"
-      hundreds_word = onesHash.fetch(num_array[2]) + " " + "hundred"
-
-      elsif num_array[1] == "0" and num_array[0].to_i >= 1
-        hundreds_word = onesHash.fetch(num_array[2]) + " hundred " + ones_word
-
-      elsif num_array[1].to_i >= 1
-        hundreds_word = onesHash.fetch(num_array[2]) + " hundred " + tens_word
+      hundreds_output_array = []
+      if self.length() == 3
+        hundreds_output_array.push(assign_tens.call(ones_and_tens_combined[1]), magnitudes[1])
       end
-      output_word = hundreds_word
-    end
-
-    if num_array.length() == 1
-      if num_array == ['0']
-        ones_word = "zero"
-      end
-      output_word = ones_word
+      output_word = hundreds_output_array.push(assign_tens.call(ones_and_tens_combined[0])).join(" ").rstrip
     end
     output_word
   end
--- spec/number_translator_spec.rb
+++ spec/number_translator_spec.rb
@@ -9,6 +9,7 @@ describe('String#initial_test') do
   it ("returns word form of value at index[0] point of array (ones digit of input number) prepended with 'negative' if value is negative ") do
     expect(('-2').initial_test()).to(eq("negative two"))
   end
+
   it ("returns word form of value for numbers with length 4-6") do
     expect(('1235').initial_test()).to(eq("one thousand two hundred thirty five"))
   end
@@ -34,34 +35,17 @@ describe('String#initial_test') do
     expect(('999999999999').initial_test()).to(eq("nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine"))
   end
   it ("returns word form of value for numbers with length 4-6") do
-    expect(('400000000000').initial_test()).to(eq("4 hundred billion"))
+    expect(('400000000000').initial_test()).to(eq("four hundred billion"))
   end
 end
-
+# #
 describe('Array#three_digit_test') do
-  # it ("returns a string with number") do
-  #   expect(12.three_digit_test()).to(eq("12"))
-  # end
-  # it ("returns an array of each digit of number") do
-  #   expect(12.three_digit_test()).to(eq(["1","2"]))
-  # end
-  # it ("returns a reverse array of each digit of number") do
-  #   expect(12.three_digit_test()).to(eq(["2","1"]))
-  # end
-  # it ("returns length of array") do
-  #   expect(12.three_digit_test()).to(eq(2))
-  # end
-  # it ("returns value at index[0] point of array (ones digit of input number)") do
-  #   expect(12.three_digit_test()).to(eq("2"))
-  # end
-  it ("returns word form of value at index[0] point of array (ones digit of input number)") do
-    expect(["2"].three_digit_test()).to(eq("two"))
-  end
+#
   it ("returns 'zero' if value equals 0") do
     expect(["0"].three_digit_test()).to(eq("zero"))
   end
-#END ONES
-
+# #END ONES
+#
   it ("returns word form of value for arrays holding numbers ten through twelve") do
     expect(['2','1'].three_digit_test()).to(eq("twelve"))
   end
@@ -74,13 +58,16 @@ describe('Array#three_digit_test') do
   it ("concatinates word-string output of numbers where index[1] equals 2-9 with word-string at index[0] to produce complete word. Inserts space between word-numbers") do
     expect(['9','9'].three_digit_test()).to(eq("ninety nine"))
   end
-#END TENS
-
+# #END TENS
+#
   it ("concatinates word-string from ones-digit hash of value at index[2] point of array with  string 'hundred'") do
     expect(['0','0','1'].three_digit_test()).to(eq("one hundred"))
   end
   it ("concatinates word-string of value at index[2] with word-string of value at index points [1] and [0]") do
     expect(['3','3','3'].three_digit_test()).to(eq("three hundred thirty three"))
+  end
+  it ("concatinates word-string of value at index[2] with word-string of value at index points [1] and [0]") do
+    expect(['5','0','2'].three_digit_test()).to(eq("two hundred five"))
   end
 #END HUNDREDS
   it ("concatinates word-string from ones-digit hash of value at index[3] point of array with  string 'thousand'") do
@@ -89,25 +76,4 @@ describe('Array#three_digit_test') do
   it ("concatinates word-string of value at index[3] with word-string of value at index points [2] through [0]") do
     expect('1205'.initial_test()).to(eq("one thousand two hundred five"))
   end
-
-# #END Ones-Class of THOUSANDS
-#   it ("returns value at index[4] point of array (tens digit of input number)") do
-#     expect(12005.translate()).to(eq(["1"]))
-#   end
-#   it ("returns word form of value for index[4].concat(index[3]) and run through teens Hash to output correct word string") do
-#     expect(12005.translate()).to(eq(["twelve"]))
-#   end
-#   it ("returns word form of value for index[4].concat(index[3]) when index[4] value is equal to 2 - 9") do
-#     expect(22.translate()).to(eq(["twenty"]))
-#   end
-#   it ("concatinates word-string output of numbers where index[4] equals 2-9 with word-string at index[3] to produce complete word. Inserts space between word-numbers") do
-#     expect(32000.translate()).to(eq(["thirty two"]))
-#   end
-#   it ("concatinates output of previous with string 'thousand'") do
-#     expect(1205.translate()).to(eq(["one thousand"]))
-#   end
-#   it ("concatinates word-string of previous with word-string of value at index points [2] through [0]") do
-#     expect(12205.translate()).to(eq(["twelve thousand two hundred five"]))
-#   end
-# #END Teens-Class of THOUSANDS
 end
